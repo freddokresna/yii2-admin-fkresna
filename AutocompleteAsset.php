4d09a75333664d3150911f895619e2757467d8b0
Freddo kresna <freddokresna@gmail.com>
2025-08-13 16:28:03 +0700

    update

--- AutocompleteAsset.php
+++ AutocompleteAsset.php
@@ -1,41 +1,23 @@
 <?php
-
 namespace mdm\admin;
 
 use yii\web\AssetBundle;
 
-/**
- * AutocompleteAsset
- *
- * @author Misbahul D Munir <misbahuldmunir@gmail.com>
- * @since 1.0
- */
 class AutocompleteAsset extends AssetBundle
 {
-    /**
-     * @inheritdoc
-     */
-    public $sourcePath = '@mdm/admin/assets';
-    /**
-     * @inheritdoc
-     */
-     public $css = [
-        // '@bower/jquery-ui/themes/base/all.css',  // hapus
-        'https://code.jquery.com/ui/1.13.3/themes/base/jquery-ui.css',
-    ];
+    // Hapus path lokal yang lama
+    // public $sourcePath = '@bower/jquery-ui';
 
     public $js = [
-        // '@bower/jquery/dist/jquery.js',         // hapus
-        // '@bower/jquery-ui/jquery-ui.js',        // hapus
-        'https://code.jquery.com/jquery-3.7.1.min.js',
-        'https://code.jquery.com/ui/1.13.3/jquery-ui.min.js',
+        // CDN jQuery UI
+        'https://code.jquery.com/ui/1.13.2/jquery-ui.min.js',
     ];
- 
-    /**
-     * @inheritdoc
-     */
+
+    public $css = [
+        'https://code.jquery.com/ui/1.13.2/themes/smoothness/jquery-ui.css',
+    ];
+
     public $depends = [
         'yii\web\JqueryAsset',
     ];
-
 }
